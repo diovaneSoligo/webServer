518abef6b1c8dbab0a0e579e02ab95aff7271a19
diovaneSoligo <diovane_soligo@hotmail.com>
2016-04-29 02:52:18 -0300

    29/04

--- src/webserver/WebServer.java
+++ src/webserver/WebServer.java
@@ -10,60 +10,125 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
-/**
- *
- * @author aluno
- */
 public class WebServer {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) throws IOException {
+    
+/*******************************************************************************/    
+    static class Buffer {
+        Queue<Tempo> queue = new LinkedList<Tempo>();
+    }
+    
+/*******************************************************************************/
+    static class XML implements Runnable {
+        Buffer buffer;
         
-//pega a hora e data no acesso
+        XML(Buffer buffer) {
+            this.buffer = buffer;
+        }
         
+        @Override
+        public void run() {
+           do{
+               System.out.println("\n\nVai atualizar o XML ...\n");
+               BuscaDadosXML XML = new BuscaDadosXML();
+               Tempo T = XML.T;
+               
+               synchronized (buffer) { 
+                   
+                            buffer.queue.add(T); // Adiciona os dados sobre a previsão do tempo
+                            buffer.notify();
+                            System.out.println("Atualizou as informações do tempo !!!\n");
+                        
+                            try {
+                                System.out.println("Vai dormir por 10 segundos ... \n");
+                                Thread.sleep(10000);//dorme por 30 segundos, depois realiza a tualização do XML
+                                }
+                            catch (InterruptedException e) {
+                                }
+                            System.out.println("acordou...\n");
+                        buffer.queue.remove();
+                        buffer.notify();
+                    }
+           }while(true);
+        }
+    }
+    
+    
+/*******************************************************************************/    
+    static class Pagina implements Runnable{
+        Buffer buffer;
+        Socket s;
         
+        Pagina(Buffer buffer, Socket s) {
+            this.buffer = buffer;
+            this.s = s;
+        }
         
-        
-        
-        ServerSocket ss = new ServerSocket(80);//porta padrão do html ...
-        //WebServer       
-        while(true){
-            Socket s = ss.accept();
-            String remoteIp = s.getInetAddress().getHostAddress();
+        @Override
+        public void run() {
+            System.out.println("está em class Pagina...\n");
             
-            //todo tramamento da , reconhecer requisição
-            
-            /*tratar tudo em thrad*/
-            
-            byte[]  b =new byte[1024]; //cria 1204 possições    ||| byte array
-            
-            int num = s.getInputStream().read(b);//se retornar -1 ja tava vazio se mais leu tudo que precisava
-            
-            if (num>0){
-                        //System.out.println(new String(b, 0, num, "ISO-8859-1"));
+            try {
+                
+                String remoteIp = s.getInetAddress().getHostAddress();
+                byte[]  b =new byte[1024]; //cria 1204 possições    ||| byte array
+                int num = s.getInputStream().read(b);//se retornar -1 ja tava vazio se mais leu tudo que precisava
+                if (num>0){
                         
                         Date data=new Date();
                         SimpleDateFormat x= new SimpleDateFormat("dd/MM/yyyy || HH:mm:ss");
-                        System.out.println("-> "+x.format(data)+" || IP CONEXÃO EXTERNA: "+remoteIp+"\n");
+                        String log = "-> "+x.format(data)+" || IP CONEXÃO EXTERNA: "+remoteIp;
+                        
+                        System.out.println(log+"\n");
+                        
+                        //gravar no log, chamar a classe GravaLog passando a string log
                       }
-            
-            //tratar requisição
-            
-            
-            //Busca os dados do clima via XML
-            BuscaDadosXML XML = new BuscaDadosXML();
-            Tempo T = XML.T;
-            
-            //mostra pagina web no navegador
-            pagina pag = new pagina(T ,s);
-            
-            
+                
+                    Tempo T = buffer.queue.peek();
+                    
+                    if(T == null){//se o buffer tiver vazio, entao espera pq ta atualizando
+                        try {
+                            System.out.println("está esperando informações serem atualizadas...\n");
+                                buffer.wait(); // Espera de as informações serem colocadas no Buffer
+                            }
+                        catch (InterruptedException e) {
+                            }
+                    }else{
+                        pagina pag = new pagina(T ,s);
+                    }
+                
             s.close();
-        }//fecha WHILE
+            
+            } catch (IOException ex) {
+                Logger.getLogger(WebServer.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            
+        }
+    }
+    
+    
+/*******************************************************************************/
+    public static void main(String[] args) throws IOException {
         
+        Buffer buffer = new Buffer();
+        
+        //abrir o aruivo TXT aqui, caso nao exista, deve-se criar o mesmo
+        
+        XML xml = new XML(buffer);
+        
+        new Thread(xml).start(); // Thread XML criada
+        ServerSocket ss = new ServerSocket(80); //porta padrão do html ...
+        
+        while(true){ //para cada requisição será criada uma nova thread
+            Socket s = ss.accept();
+            Pagina pag = new Pagina(buffer, s/*txtLog*/ );//passar o arquivo txt aberto para a class Pagina
+            new Thread(pag).start(); // Thread pagina criada 
+        }
+
     }
     
 }
